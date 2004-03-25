18239e274b0fa83266166e35b8582399cfcf93e6
martinkl <martinkl>
2004-03-25 10:06:40 +0000

    test-programs updated

--- test/test_c.c
+++ test/test_c.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "libopensteuer.h"
+#include <libopensteuer.h>
 
 int main(void)
 {
   long r, lstlzz;
-  int stkl;
-
-  printf ("Sprotzel.. Hier ist das C-Programm...\n");
-  printf ("     RE4       I      II     III      IV       V      VI\n");
-  printf ("--------------------------------------------------------\n");
-  for (r = 2; r <=24; ++r) {
-    printf ("%8ld", r * 2500);
-    for (stkl = 1; stkl <= 6; ++stkl) {
-      reset_all ();
-      set_lzz (1);
-      set_re4 (r * 250000);
-      set_stkl (stkl);
-      calc_lst ();
-      lstlzz = get_lstlzz () / 100;
-      
-      printf("%8ld", lstlzz);
-    };
-    printf ("\n");
+  int stkl, year;
+  
+  printf ("\nUnd hier das C-Programm...\n");
+  for (year = get_first_year (); year <= get_last_year (); ++year) {
+    printf ("\nJahr: %d\n", year);
+    printf ("     RE4       I      II     III      IV       V      VI\n");
+    printf ("--------------------------------------------------------\n");
+    for (r = 2; r <=24; ++r) {
+      printf ("%8ld", r * 2500);
+      for (stkl = 1; stkl <= 6; ++stkl) {
+        reset_all ();
+        switch (set_year (year)) {
+        case 0 :
+          set_lzz (1);
+          set_re4 (r * 250000);
+          set_stkl (stkl);
+          calc_lst ();
+          lstlzz = get_lstlzz () / 100;
+          printf ("%8ld", lstlzz);
+          break;
+        case -1 :
+          printf (" general");
+          break;
+        case -2 :
+          printf (" program");
+          break;
+        case -3 :
+          printf ("   range");
+          break;
+        case -10 :
+          printf ("   undef");
+          break;
+        default :
+          printf (" default");
+          break;
+        };
+      };
+      printf ("\n");
+    }
   }
   return 0;
 }
