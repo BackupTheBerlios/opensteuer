ee733ff9a26bfef0f7d557c7f9e52a22d9ffd282
martinkl <martinkl>
2004-03-29 17:46:29 +0000

    Calculating the Grund- and Splittingtab included into the testsuite.

--- test/test_ada.adb
+++ test/test_ada.adb
@@ -6,7 +6,9 @@ procedure test_ada is
    First_Year, Last_Year : Ada_Integer;
 begin
    New_Line;
-   Put_Line ("Hier meldet sich das Ada-Programm...");
+   Put_Line ("Hier ist das Ada-Programm...");
+   New_Line;
+   Put_Line ("Lohnsteuerberechnung (alle Beträge in Euro):");
    First_Year := Get_First_Year;
    Last_Year := Get_Last_Year;
    for Jahr in First_Year .. Last_Year loop
@@ -24,8 +26,28 @@ begin
             Set_STKL (Ada_Integer (i));
             Calc_Lst;
             Put (Tail (Ada_Cent'Image (Get_LSTLZZ / 100), 8));
-         end loop;
+      end loop;
+      New_Line;
+      end loop;
+   end loop;
+   New_Line;
+   Put_Line ("Einkommensteuerberechnung (alle Beträge in Euro):");
+   for Jahr in First_Year .. Last_Year loop
+      New_Line;
+      Put_Line ("Jahr:" & Ada_Integer'Image (Jahr));
+      Put_Line ("     ZVE        Grundtabelle     Splittingtabelle");
+      Put_Line ("-------------------------------------------------");
+      for r in 1 .. 10 loop
+         Reset_All;
+         Set_Year (Ada_Integer (Jahr));
+         Set_LZZ (1);
+         Set_ZVE (Ada_Cent (r) * 10_000_00);
+         Put (Tail (Ada_Cent'Image (Ada_Cent (r) * 10_000), 8));
+         Calc_Est;
+         Put (Tail (Ada_Cent'Image (Get_Grundtab / 100), 20));
+         Put (Tail (Ada_Cent'Image (Get_Splittab / 100), 20));
          New_Line;
       end loop;
    end loop;
+   New_Line;
 end test_ada;
--- test/test_c.c
+++ test/test_c.c
@@ -6,44 +6,67 @@ int main(void)
 {
   long r, lstlzz;
   int stkl, year;
-  
+
   printf ("\nUnd hier das C-Programm...\n");
-  for (year = get_first_year (); year <= get_last_year (); ++year) {
-    printf ("\nJahr: %d\n", year);
-    printf ("     RE4       I      II     III      IV       V      VI\n");
-    printf ("--------------------------------------------------------\n");
-    for (r = 2; r <=24; ++r) {
-      printf ("%8ld", r * 2500);
-      for (stkl = 1; stkl <= 6; ++stkl) {
-        reset_all ();
-        switch (set_year (year)) {
-        case 0 :
-          set_lzz (1);
-          set_re4 (r * 250000);
-          set_stkl (stkl);
-          calc_lst ();
-          lstlzz = get_lstlzz () / 100;
-          printf ("%8ld", lstlzz);
-          break;
-        case -1 :
-          printf (" general");
-          break;
-        case -2 :
-          printf (" program");
-          break;
-        case -3 :
-          printf ("   range");
-          break;
-        case -10 :
-          printf ("   undef");
-          break;
-        default :
-          printf (" default");
-          break;
-        };
-      };
-      printf ("\n");
+  printf ("\nLohnsteuerberechnung (alle Beträge in Euro):\n");
+  for (year = get_first_year (); year <= get_last_year (); ++year)
+    {
+      printf ("\nJahr: %d\n", year);
+      printf ("     RE4       I      II     III      IV       V      VI\n");
+      printf ("--------------------------------------------------------\n");
+      for (r = 2; r <=24; ++r)
+        {
+          printf ("%8ld", r * 2500);
+          for (stkl = 1; stkl <= 6; ++stkl)
+            {
+              reset_all ();
+              switch (set_year (year))
+                {
+                case 0 :
+                  set_lzz (1);
+                  set_re4 (r * 250000);
+                  set_stkl (stkl);
+                  calc_lst ();
+                  lstlzz = get_lstlzz () / 100;
+                  printf ("%8ld", lstlzz);
+                  break;
+                case -1 :
+                  printf (" general");
+                  break;
+                case -2 :
+                  printf (" program");
+                  break;
+                case -3 :
+                  printf ("   range");
+                  break;
+                case -10 :
+                  printf ("   undef");
+                  break;
+                default :
+                  printf (" default");
+                  break;
+                };
+            };
+          printf ("\n");
+        }
     }
-  }
+  printf ("\nEinkommensteuerberechnung (alle Beträge in Euro):\n");
+  for (year = get_first_year (); year <= get_last_year (); ++year)
+    {
+      printf ("\nJahr: %d\n", year);
+      printf ("     ZVE        Grundtabelle    Splittingtabelle\n");
+      printf ("------------------------------------------------\n");
+      for (r = 1; r <=10; ++r)
+        {
+          printf ("%8ld", r * 10000);
+          reset_all ();
+          set_year (year);
+          set_lzz (1);
+          set_zve (r * 1000000);
+          calc_est ();
+          printf ("%20ld", get_grundtab () / 100);
+          printf ("%20ld\n", get_splittab () / 100);
+        };
+    }
   return 0;
 }
